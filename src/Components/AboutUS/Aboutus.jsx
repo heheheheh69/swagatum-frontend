d6dc7e981ebdf2f273ad2a5e220ff141f68132cb
Priyanshu <priyanshukashyap844@gmail.com>
2025-04-29 13:54:36 +0530

    .

--- src/Components/AboutUS/Aboutus.jsx
+++ src/Components/AboutUS/Aboutus.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import "./Aboutus.css"
 const Aboutus = () => {
-  return (
-    <>
-        <div className="aboutus">
-            <div className="container">
-                
-                <div className="aboutus-content">
-                    <img src="./src/assets/about.jpeg" alt="" />
-                    <div className="aboutus-txt">
-                    <h2>
-                        About Us
-                    </h2>
-                        <h3>who we are ?</h3>
-                        <p>At Swagatum, we believe that a beautiful space starts from the ground up. Our mission is to bring warmth, style, and comfort into your home through thoughtfully crafted carpets. Whether you're designing a modern living space or adding a cozy touch to a traditional room, our diverse range of carpets is designed to complement every lifestyle.</p>
-                        <span><b>Ready to learn more or get in touch? We're here to help—reach out to us today and let’s create something amazing together.</b></span>
-                        <button className='main-btn'><a href="/about">Aboutus</a></button>
+    return (
+        <>
+            <div className="aboutus">
+                <div className="container">
+
+                    <div className="aboutus-content">
+                        <img src="./src/assets/about.jpeg" alt="" />
+                        <div className="aboutus-txt">
+                            <div className="head">
+                                <h2>
+                                    About Us
+                                </h2>
+                                <h3>who we are ?</h3>
+                            </div>
+
+                            <p>At Swagatum, we believe that a beautiful space starts from the ground up. Our mission is to bring warmth, style, and comfort into your home through thoughtfully crafted carpets. Whether you're designing a modern living space or adding a cozy touch to a traditional room, our diverse range of carpets is designed to complement every lifestyle. <br /><br />
+                                <b>Ready to learn more or get in touch? We're here to help—reach out to us today and let’s create something amazing together.</b></p>
+                                
+                            <button className='main-btn'><a href="/about">Aboutus</a></button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div> 
-    </>
-  )
+        </>
+    )
 }
 
 export default Aboutus;
--- src/Components/WhyUs/WhyUs.jsx
+++ src/Components/WhyUs/WhyUs.jsx
@@ -12,10 +12,13 @@ const WhyUs = () => {
                     <img src="./src/assets/about.jpeg" alt="" />
                     </div>
                     <div className="aboutus-txt">
+                        <div className="head">
+
                     <h2>
                         Why Choose Us
                     </h2>
                         <h3>What makes us different</h3>
+                        </div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et facilisis augue. Aenean aliquam massa non lacus consequat rhoncus. Pellentesque tempus dui eu mi auctor semper et sit amet sapien. Morbi dapibus ante sem, id ullamcorper felis condimentum vulputate. Donec ut nisi facilisis, lacinia magna</p>
                         <ul className='why-list'>
                             <li><span>Lorem ipsum dolor sit amet, consectetur </span></li>
