910684e32d77af1125902a0d091f3cdb742f20b5
Priyanshu <priyanshukashyap844@gmail.com>
2025-05-26 00:12:03 +0530

    .

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -30,11 +30,7 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="footer-section">
-          <h4>Contact Us</h4>
-          <p>Email: support@example.com</p>
-          <p>Phone: +1 234 567 890</p>
-        </div>
+        
       </div>
 
       <div className="footer-bottom">
